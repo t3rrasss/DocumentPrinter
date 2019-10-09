101422ce131f0e3615148b1e0ff47455981c6f53
t3rrasss <56136211+t3rrasss@users.noreply.github.com>
2019-10-09 16:27:08 +0100

    2 Solutions to the proposed task

---
+++ DocumentPrinter.cs
@@ -0,0 +1,77 @@
+﻿namespace DocumentPrinter
+{
+    //By introducing IHPInterface with its method to print, we will not be breaking existing code
+    //where this method is already being called. By introducing extra interfaces, we'tr adding extra
+    //functionality to our class to be able to print to other printers. At the same time we're making sure our
+    //class is following the open-closed principle of SOLID.
+    public class DocumentPrinter : IHPInterface, IDellInterface, IAsusInterface
+    {
+
+        public DocumentPrinter()
+        {
+
+        }
+
+        /// <summary>
+        /// One public method that accept a document to print
+        /// Assume this is used on several places of the code
+        /// </summary>
+        /// <param name="document"></param>
+        public void PrintDocument(object document)
+        {
+            var formattedDoc = FormatDocument(document);
+            PrintToHp(formattedDoc);
+        }
+
+
+        private object FormatDocument(object document)
+        {
+            // Does some formatting. E.g. Add page numbers
+            object formattedDocument = document;
+            return formattedDocument;
+        }
+
+
+        public void PrintToHp(object document)
+        {
+            // Some code written to print a document to an HP printer
+            // Note: The exercise is not aimed at actually printing documents,
+            // so please assume there is no standard api, so this method
+            // cannot be reused to print on other printer models
+        }
+
+        //right now these methods are not used, but we could add some methods similar to 
+        //PrintDocument to call the bellow two as needed. Another solution i was thinking about
+        //is to 
+        public void PrintToDell(object document)
+        {
+            //some code to print to dell
+        }
+
+        public void PrintToAsus(object document)
+        {
+            //some code to print to asus
+        }
+    }
+
+
+
+    //We could have created a single interface which has all these methods inside,
+    //but by doing so we would be forcing all the functionality of the interface and
+    //this class may not necessarily need all of it. By doing interface segregation
+    //we're giving more freedom as to what functionality this class needs to implement
+    public interface IHPInterface
+    {
+        void PrintToHp(object document);
+    }
+
+    public interface IDellInterface
+    {
+        void PrintToDell(object document);
+    }
+
+    public interface IAsusInterface
+    {
+        void PrintToAsus(object document);
+    }
+}
---
+++ DocumentPrinter2.cs
@@ -0,0 +1,87 @@
+﻿namespace DocumentPrinter
+{
+    //This aproach is different than my previous one and would require us to change code
+    //wherever this class is being used, therefor I think its the the optimal solution.
+    // The idea here is that we used dependancy injection
+    //to inject a class which implements an IPrinter interface. The place using this class,
+    //would have to pass a desired printer object as parameter to the constructor.
+    public class DocumentPrinter2
+    {
+        private IPrinter _iPrinter;
+
+        public DocumentPrinter2(IPrinter iPrinter)
+        {
+            _iPrinter = iPrinter;
+        }
+
+        /// <summary>
+        /// One public method that accept a document to print
+        /// Assume this is used on several places of the code
+        /// </summary>
+        /// <param name="document"></param>
+        public void PrintDocument(object document)
+        {
+            var formattedDoc = FormatDocument(document);
+            _iPrinter.Print(formattedDoc); //this would call the print method of whichever printer object was passed
+        }
+
+
+        private object FormatDocument(object document)
+        {
+            // Does some formatting. E.g. Add page numbers
+            object formattedDocument = document;
+            return formattedDocument;
+        }
+
+
+        //The implementation of this method would be moved to HPPrinter class's print method
+        public void PrintToHp(object document)
+        {
+            // Some code written to print a document to an HP printer
+            // Note: The exercise is not aimed at actually printing documents,
+            // so please assume there is no standard api, so this method
+            // cannot be reused to print on other printer models
+        }
+    }
+
+    //our interface with the print method
+    public interface IPrinter
+    {
+        void Print(object document);
+    }
+
+    //Printer class which implement the interface
+    public class AsusPrinter : IPrinter
+    {
+        public void Print(object document)
+        {
+            //code to print to asus printer
+        }
+    }
+
+    //Printer class which implement the interface
+    public class HPPrinter : IPrinter
+    {
+        public void Print(object document)
+        {
+            //code to print to HP printer
+        }
+    }
+
+    //This is a class which would require to print a document
+    public class PlaceWePrintFrom {
+
+        private void DoSomething()
+        {
+            Object ourPrintObject = new object();
+
+            //Print to Asus
+            DocumentPrinter2 dp2 = new DocumentPrinter2(new AsusPrinter());
+            dp2.PrintDocument(ourPrintObject);
+
+            //Print to Asus
+            DocumentPrinter2 dp2_2 = new DocumentPrinter2(new HPPrinter());
+            dp2.PrintDocument(ourPrintObject);
+        }
+    }
+}
